86a1d3d1a15dcda4d8b6477c23ea055f263e12a7
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-17 12:21:45 +0530

    updated

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ ENABLED_GROUPS = set()  # set of chat IDs where auto-approve is enabled
 
 @ryme.on_message(filters.private & filters.command('start'))
 async def start(client, message):
-    await message.reply(f"<b>Hello {message.from_user.mention},</b>\n\n<b>Welcome! I'm here to help you manage your group by automatically approving new members. To get started:</b>\n\n1. Please add me to your group.\n2. Ensure I have administrative rights with full permissions.\n3. Once that's set up, just send the command</b> <code>/approve on</code> in the group, and I'll handle the rest.")
+    await message.reply(f"<b>Hello {message.from_user.mention},</b>\n\n<b>Welcome! I'm here to help you manage your group by automatically approving new members. To get started:</b>\n\n1. Please add me to your group.\n2. Ensure I have administrative rights with full permissions.\n3. Once that's set up, just send the command /approve in the group, and I'll handle the rest.")
 
 @ryme.on_chat_join_request(filters.group | filters.channel)
 async def autoapprove(client: Client, message: ChatJoinRequest):
@@ -26,11 +26,16 @@ async def autoapprove(client: Client, message: ChatJoinRequest):
         await client.approve_chat_join_request(chat_id=chat_id, user_id=user.id)
         await client.send_message(chat_id=chat_id, text=TEXT.format(user.mention, message.chat.title))
 
-@ryme.on_message(filters.command("approve") & filters.group)
+@ryme.on_message(filters.command("approve"))
 async def toggle_autoapprove(client: Client, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    
+     
+    # Check if the message is from a private chat
+    if message.enums.ChatType.PRIVATE == True:
+        await message.reply("This command can only be used in groups.")
+        return
+
     administrators = []
     async for m in client.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
         administrators.append(m.user.id)
@@ -38,7 +43,7 @@ async def toggle_autoapprove(client: Client, message: Message):
     if user_id not in administrators:
         await message.reply("Only group admins can enable or disable auto approve.")
         return
-
+    
     status = "enabled" if chat_id in ENABLED_GROUPS else "disabled"
     markup = InlineKeyboardMarkup([
         [InlineKeyboardButton("ON", callback_data="autoapprove_on"),
